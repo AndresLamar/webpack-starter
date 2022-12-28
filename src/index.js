6735025bf6f9c196d9a4c34c93d2a7ddb16e90cd
Andres <andresisfelipe20@gmail.com>
2022-12-28 16:03:59 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-
-
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Andrés';
 
-saludar( nombre );
+export const todoList = new TodoList();
 
+todoList.todos.forEach(crearTodoHtml);
+
+/* const tarea = new Todo('Aprender JavaScript');
+todoList.nuevoTodo( tarea );
+
+crearTodoHtml( tarea );  
+
+localStorage.setItem('mi-key', 'ABC123');
+sessionStorage.setItem('mi-key', 'ABC123');
+ */
+/* setTimeout( ()=> {
+
+    localStorage.removeItem('mi-key');
+
+}, 1500 ); */
